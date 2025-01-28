1f392a7c1554e8f73c5b5811708bc18968fb35a3
dagrif1223 <dagrif1223@gmail.com>
2025-01-27 21:18:14 -0500

    integrated cloud sql

--- knowledgebase_app/app.py
+++ knowledgebase_app/app.py
@@ -1,33 +1,34 @@
-from flask import Flask, render_template
-from flask import Flask,render_template, request
-from flask_mysqldb import MySQL
+from flask import Flask, render_template, request, make_response, flash, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
+import logging
 
+# initializing Flask app
 app = Flask(__name__)
 
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'seniorproject'
-app.config['MYSQL_DB'] = 'test'
+# Google Cloud SQL configuration
+PASSWORD = "seniorproject"
+PUBLIC_IP_ADDRESS = "34.45.162.72"
+DBNAME = "test"
+PROJECT_ID = "regal-reporter-449223-a4"
+INSTANCE_NAME = "knowledgebase"
 
-mysql = MySQL(app)
- 
-#Creating a connection cursor (Testing the connection)
-# with app.app_context():
-#     cursor = mysql.connection.cursor()
-    
-#     #Executing SQL Statements
-#     cursor.execute(''' DROP TABLE IF EXISTS test_table ''')
-#     cursor.execute(''' CREATE TABLE test_table (test_field1 INT, test_field2 VARCHAR(100)) ''')
-#     cursor.execute(''' INSERT INTO test_table (test_field1, test_field2) VALUES (%s, %s) ''', (1, 2))
+# configuration
+app.config["SECRET_KEY"] = "yoursecretkey"
+app.config["SQLALCHEMY_DATABASE_URI"] = f"mysql+mysqldb://root:{PASSWORD}@{PUBLIC_IP_ADDRESS}/{DBNAME}?unix_socket=/cloudsql/{PROJECT_ID}:{INSTANCE_NAME}"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 
+db = SQLAlchemy(app)
 
-    
-#     #Saving the Actions performed on the DB
-#     mysql.connection.commit()
-    
-#     #Closing the cursor
-#     cursor.close()
+# User ORM for SQLAlchemy
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    fname = db.Column(db.String(50), nullable=False)
+    lname = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(50), nullable=False, unique=True)
+    password = db.Column(db.String(200), nullable=False)
+    school = db.Column(db.String(50), nullable=False)
+    user_type = db.Column(db.String(50), nullable=False)
 
 @app.route('/')
 def home():
@@ -36,7 +37,7 @@ def home():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template('login.html')
-    
+
 @app.route('/about')
 def about():
     return render_template('about.html')
@@ -46,18 +47,47 @@ def register():
     if request.method == 'POST':
         # Fetch form data
         userDetails = request.form
+        fname = userDetails['fname']
+        lname = userDetails['lname']
+        email = userDetails['email']
         password = userDetails['password']
+        school = userDetails['school']
+        user_type = userDetails['user-type']
         # Hash the password
         hashed_password = generate_password_hash(password)
-        email = userDetails['email']
-        cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO users(email, password) VALUES(%s, %s)",(email, hashed_password))
-        mysql.connection.commit()
-        cur.close()
+        
+        # checking if user already exists
+        user = Users.query.filter_by(email=email).first()
+
+        if not user:
+            try:
+                # creating Users object
+                user = Users(
+                    fname=fname,
+                    lname=lname,
+                    email=email,
+                    password=hashed_password,
+                    school=school,
+                    user_type=user_type
+                )
+                # adding the fields to users table
+                db.session.add(user)
+                db.session.commit()
+                # flash success message
+                flash('Successfully registered.', 'success')
+                return redirect(url_for('register'))
+            except Exception as e:
+                logging.error(f"Error occurred: {e}")
+                flash('Some error occurred!!', 'danger')
+                return redirect(url_for('register'))
+        else:
+            # if user already exists then send status as fail
+            flash('User already exists!!', 'warning')
+            return redirect(url_for('register'))
 
     return render_template('register.html')
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
-
-    
