e08df7f449bc1846e91311622195338c50ea81ad
sariahadams <adamssariah2@gmail.com>
2025-01-28 13:06:09 -0500

    progress

--- knowledgebase_app/app.py
+++ knowledgebase_app/app.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, redirect, url_for, session, request, flash
 
 app = Flask(__name__)
+
+#stimulated user database
+users = {}
 
 @app.route('/')
 def home():
@@ -9,8 +11,28 @@ def home():
 
 @app.route('/login')
 def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        user = users.get(email, None)
+        
+        if user and user['password'] == password:
+            session['user'] = user
+            flash('Login Successful!', 'success')
+            return redirect(url_for('profile'))
+        else:
+            flash('Login Failed! Please check your credentials', 'danger')
     return render_template('login.html')
+
+@app.route('/register')
+def register():
     
+    return render_template('register.html')
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
 @app.route('/about')
 def about():
     return render_template('about.html')
