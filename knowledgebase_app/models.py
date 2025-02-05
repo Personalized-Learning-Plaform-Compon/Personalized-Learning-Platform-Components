d352e3cf2b69d94aa96a28a0f7acd115364eab55
jjackson15 <jacksonjennai81503@gmail.com>
2025-02-04 19:46:22 -0500

    changed hash method

--- knowledgebase_app/models.py
+++ knowledgebase_app/models.py
@@ -17,7 +17,7 @@ class User(UserMixin, db.Model):
     user_type = db.Column(db.String(50), nullable=False)
     
     def set_password(self, password):
-        self.password = generate_password_hash(password)
+        self.password = generate_password_hash(password, method='pbkdf2:sha256')
     
     def check_password(self, password):
         return check_password_hash(self.password, password)
